2aff1c23d1b6df7d83adfbf1d443c3f14a2d7f39
Sowjanya-83 <sowjanyaks83@gmail.com>
2026-02-03 12:01:55 +0530

    day2

---
+++ src/concantination.py
@@ -0,0 +1,9 @@
+"""a=input("enter first string")
+b=input("enter second string")
+print("concate",a+b)
+"""""
+
+name="sowjanya"
+age="22"
+print("my name is {} and my age is {}" .format(name,age))
+
