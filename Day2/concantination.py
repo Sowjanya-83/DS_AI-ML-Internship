25a28436fe9fd90b70444efa5772ab32108bfd5d
Sowjanya K S <165817808+Sowjanya-83@users.noreply.github.com>
2026-02-04 13:03:46 +0530

    Delete Day2 directory

--- Day2/concantination.py
+++
@@ -1,9 +0,0 @@
-"""a=input("enter first string")
-b=input("enter second string")
-print("concate",a+b)
-"""""
-
-name="sowjanya"
-age="22"
-print("my name is {} and my age is {}" .format(name,age))
-
