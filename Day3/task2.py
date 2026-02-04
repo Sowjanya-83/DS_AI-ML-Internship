3cf1fb2814ac54ef64a755642f5e1aed770c8fc8
Sowjanya-83 <sowjanyaks83@gmail.com>
2026-02-04 11:05:47 +0530

    day2

---
+++ Day3/task2.py
@@ -0,0 +1,6 @@
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print("Original temperatures:", temperatures)
+print("first reading:", temperatures[0])
+print("last reading:", temperatures[-1])
+print("afternoon peak:",temperatures[3:6])
+print("Last 3 Hours:",temperatures[-3:])
