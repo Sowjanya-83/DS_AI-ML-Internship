d85b5a86ea0ed0f4e29435327e40e5f2d24194d6
Sowjanya K S <165817808+Sowjanya-83@users.noreply.github.com>
2026-02-04 13:03:26 +0530

    Delete Day3 directory

--- Day3/task1.py
+++
@@ -1,8 +0,0 @@
-inventory=["Apples", "Bananas", "Carrots", "Dates"]
-print("current inventory:",inventory)
-inventory.append("egg")
-print(inventory)
-inventory.remove("Bananas")
-print(inventory)
-inventory.sort()
-print("final inventory:", inventory)
--- Day3/task3.py
+++
@@ -1,4 +0,0 @@
-screen_res = (1920, 1080)
-print("current screen resolution:", screen_res[0],"x", screen_res[1])
-"""screen_res [0]=1280"""
-print("Tuples cannot be modified!")
