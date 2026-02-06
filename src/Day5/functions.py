a2b4b18639cd97fcf431517fcd37495f2df5b874
Sowjanya-83 <sowjanyaks83@gmail.com>
2026-02-06 12:54:55 +0530

    Initial commit

---
+++ src/Day5/functions.py
@@ -0,0 +1,34 @@
+
+"""def greet():
+    print("Hello, welcome to the internship program!")
+greet()
+"""
+"""def add(a, b):
+    return a + b
+result=add(5, 3)
+print("The sum is:", result)
+"""
+"""x=10
+def show_value():
+    x=5
+    print(x)
+show_value()
+print(x)
+"""
+"""
+name="sowjanya"
+def display_name():
+    name="sowjanya"
+    age=22
+print("Name:", name)
+display_name()
+print("Age:",age)
+"""
+
+import math
+import random
+print(math.sqrt(16))
+print(random.randint(1,10))
+
+
+    
---
+++ src/Day5/task2/main.py
@@ -0,0 +1,5 @@
+import util
+result_power =util.power(2, 10)
+result_average = util.average([10, 20, 30, 40])
+print("2^10 =", result_power)
+print("Average =", result_average)
