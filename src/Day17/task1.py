5db38137b10437cb2aa5941016c0e8463100a50d
Sowjanya-83 <sowjanyaks83@gmail.com>
2026-02-23 10:32:02 +0530

    initial commit

--- src/Day17/task1.py
+++ src/Day17/task1.py
@@ -1,10 +1,6 @@
 import sqlite3
-
-# 1. Connect to (or create) the database
 conn = sqlite3.connect("internship.db")
 cur = conn.cursor()
-
-# 2. Create the interns table
 cur.execute("""
 CREATE TABLE IF NOT EXISTS interns (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,8 +9,6 @@ CREATE TABLE IF NOT EXISTS interns (
     stipend INTEGER
 );
 """)
-
-# 3. Insert sample data
 cur.execute("INSERT INTO interns (name, track, stipend) VALUES ('Aarav', 'Data Science', 12000)")
 cur.execute("INSERT INTO interns (name, track, stipend) VALUES ('Meera', 'Web Development', 10000)")
 cur.execute("INSERT INTO interns (name, track, stipend) VALUES ('Rohan', 'AI & ML', 15000)")
@@ -22,8 +16,6 @@ cur.execute("INSERT INTO interns (name, track, stipend) VALUES ('Sanya', 'Cyber 
 cur.execute("INSERT INTO interns (name, track, stipend) VALUES ('Kiran', 'Data Analytics', 13000)")
 
 conn.commit()
-
-# 4. SELECT only name and track (NOT SELECT *)
 print("Intern Names and Tracks:")
 cur.execute("SELECT name, track FROM interns")
 rows = cur.fetchall()
